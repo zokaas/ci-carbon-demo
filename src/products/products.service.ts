cc8bdde953a199583c4461abbd423a3edf987a38
Zorana Bozic <bozic.zorana@gmail.com>
2026-05-09 16:39:09 +0300

    feat: docker simulation commit [34/40]

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -53,3 +53,4 @@ export class ProductsService {
 // docker-sim commit 25 - Sat May  9 16:02:56 EEST 2026
 // docker-sim commit 28 - Sat May  9 16:15:00 EEST 2026
 // docker-sim commit 31 - Sat May  9 16:27:04 EEST 2026
+// docker-sim commit 34 - Sat May  9 16:39:09 EEST 2026
